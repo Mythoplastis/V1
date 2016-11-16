ac2e9572476a4f973d616860d1cbdd8c7fd226b5
moozhansh <moozhansh@gmail.com>
2016-11-16 17:10:38 +0000

    First Harp

--- public/assets/js/flag.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    var backgroundAudio=document.getElementById("sound");
-    backgroundAudio.volume=0.2;
-}
--- public/libs/link/js/newAnimOnScroll.js
+++
@@ -1,5 +0,0 @@
-      new AnimOnScroll( document.getElementById( 'grid' ), {
-      minDuration : 0.4,
-      maxDuration : 0.7,
-      viewportFactor : 0.2
-      } );
